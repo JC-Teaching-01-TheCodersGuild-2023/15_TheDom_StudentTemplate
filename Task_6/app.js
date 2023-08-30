84fbfb20d1c925c3b90f736b1261897e2d63778f
Sam <samnewell91@gmail.com>
2023-08-30 16:25:58 +0100

    All tasks completed

--- Task_6/app.js
+++ Task_6/app.js
@@ -5,16 +5,11 @@ const priorityImages = {
   high: 'https://cdn1.iconfinder.com/data/icons/prettyoffice8/256/Flag-red.png',
 };
 
-// Get the form by ID from the forms collection.
-const form = false;
-// Get the todo pane (the 'ul' element) to insert todos into.
-const todoPane = false;
-// Get the text input for the title. We'll read from this when creating the todo.
-const titleInput = false;
-// Get the priority select element. We'll read from this when creating the todo.
-const prioritySelect = false;
-// Get a *live* list of all elements with the 'todo' class.
-const allTodos = false;
+
+const form = document.getElementById('todo');
+const todoPane = document.getElementById('todo-pane');
+const prioritySelect = document.querySelectorAll('.priority').value;
+const allTodos = document.getElementsByClassName('todo');
 
 // Add an event listener that will
 // 1. Create a new todo. The details should come from the form.
@@ -22,15 +17,30 @@ const allTodos = false;
 // 3. Clear the title input ready to create a new todo note.
 // 4. Prevent the default behaviour (ie don't submit to a server).
 
+form.addEventListener('submit', function(e) {
+  for (const todo of allTodos){
+    todo.classList.remove('just-created');
+  }
+  const titleInput = document.getElementById('title').value;
+  const newTodo = createTodo(titleInput);
+  todoPane.appendChild(newTodo);
+  e.preventDefault();
+  form.reset();
+})
+
 function createTodo(title) {
-  // Create the text node with the variable 'title'.
-  
-  // Create a new list item element to contain the text node.
-  
-  // Add the text node to the list item element.
-  
-  // Return the new element.
+const newText = document.createTextNode(title);
+const newListItem = document.createElement('li');
+newListItem.classList.add('todo', 'just-created')
+newListItem.appendChild(newText);
+newListItem.addEventListener('click', function(e) {
+  e.currentTarget.remove();
+})
+return newListItem 
+
 }
+
+
 
 // The most recently created list item (only!) should have the 'just-created' class.
 
